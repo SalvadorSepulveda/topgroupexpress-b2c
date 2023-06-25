39125c55a98c2bf6e59120600cb31cd3d518c24a
Roberto Antonio Becerra García <idertator@gmail.com>
2023-06-25 22:41:39 +0200

    feat: Added social to footer

--- src/components/icons/TGEIcon.vue
+++ src/components/icons/TGEIcon.vue
@@ -4,18 +4,23 @@
 
 <script>
 import AppsIcon from './AppsIcon.vue'
+import FacebookIcon from './FacebookIcon.vue'
 import LayersIcon from './LayersIcon.vue'
 import LayoutIcon from './LayoutIcon.vue'
+import LinkedInIcon from './LinkedInIcon.vue'
 import ReloadIcon from './ReloadIcon.vue'
 import SlidersIcon from './SlidersIcon.vue'
 import SpeedIcon from './SpeedIcon.vue'
+import TwitterIcon from './TwitterIcon.vue'
 
 export default {
     name: 'TGEIcon',
     components: {
         AppsIcon,
+        FacebookIcon,
         LayersIcon,
         LayoutIcon,
+        LinkedInIcon,
         ReloadIcon,
         SlidersIcon,
         SpeedIcon,
@@ -31,16 +36,22 @@ export default {
             switch (this.name) {
                 case 'apps':
                     return AppsIcon
+                case 'facebook':
+                    return FacebookIcon
                 case 'layers':
                     return LayersIcon
                 case 'layout':
                     return LayoutIcon
+                case 'linkedin':
+                    return LinkedInIcon
                 case 'reload':
                     return ReloadIcon
                 case 'sliders':
                     return SlidersIcon
                 case 'speed':
                     return SpeedIcon
+                case 'twitter':
+                    return TwitterIcon
             }
             return undefined
         },
