3478eef202cf67d9724375009bff8e05dc773e4b
Roberto Antonio Becerra García <idertator@gmail.com>
2023-06-25 17:38:06 +0200

    feat: Cleaned some code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -678,7 +678,7 @@ pageLink.forEach((elem) => {
 function onScroll(event) {
     const sections = document.querySelectorAll('.menu-scroll')
     const scrollPos =
-        window.pageYOffset ||
+        window.scrollY ||
         document.documentElement.scrollTop ||
         document.body.scrollTop
 
@@ -806,36 +806,6 @@ GLightbox({
     })
 
     /* ========  mobile menu end ========= */
-
-    /* ========  scroll to top  start ========= */
-    function scrollTo(element, to = 0, duration = 500) {
-        const start = element.scrollTop
-        const change = to - start
-        const increment = 20
-        let currentTime = 0
-
-        const animateScroll = () => {
-            currentTime += increment
-
-            const val = Math.easeInOutQuad(currentTime, start, change, duration)
-
-            element.scrollTop = val
-
-            if (currentTime < duration) {
-                setTimeout(animateScroll, increment)
-            }
-        }
-
-        animateScroll()
-    }
-
-    Math.easeInOutQuad = function (t, b, c, d) {
-        t /= d / 2
-        if (t < 1) return (c / 2) * t * t + b
-        t--
-        return (-c / 2) * (t * (t - 2) - 1) + b
-    }
-    /* ========  scroll to top  end ========= */
 })()
 
 import TGEHeroSection from '@/components/TGEHeroSection.vue'
