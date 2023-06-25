a204000989a2fd3f2c390de139210a23fde49fef
Roberto Antonio Becerra García <idertator@gmail.com>
2023-06-25 19:05:22 +0200

    feat: Removed side circles

--- src/components/TGEScreensSection.vue
+++ src/components/TGEScreensSection.vue
@@ -153,44 +153,6 @@
                 </div>
             </div>
         </div>
-
-        <!-- Graphics -->
-        <div class="absolute left-0 top-0 -z-10">
-            <svg
-                width="95"
-                height="190"
-                viewBox="0 0 95 190"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <circle
-                    cy="95"
-                    r="77"
-                    stroke="url(#paint0_linear_47_26)"
-                    stroke-width="36"
-                />
-                <defs>
-                    <linearGradient
-                        id="paint0_linear_47_26"
-                        x1="-117.84"
-                        y1="190"
-                        x2="117.828"
-                        y2="25.9199"
-                        gradientUnits="userSpaceOnUse"
-                    >
-                        <stop stop-color="#8EA5FE" />
-                        <stop
-                            offset="0.541667"
-                            stop-color="#BEB3FD"
-                        />
-                        <stop
-                            offset="1"
-                            stop-color="#90D1FF"
-                        />
-                    </linearGradient>
-                </defs>
-            </svg>
-        </div>
     </TGESection>
 </template>
 
