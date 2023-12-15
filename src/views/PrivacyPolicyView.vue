475f3762733ecf03ba23b1251945fe29eab02509
Salvador Sepúlveda Millán <ssepmil0110@gmail.com>
2023-12-15 17:47:55 +0100

    feat: added provacity

---
+++ src/views/PrivacyPolicyView.vue
@@ -0,0 +1,121 @@
+<template>
+	<div class="w-full flex justify-center p-[3rem]">
+		<div class="max-w-[1500px]">
+			<h1>
+				{{ $t('titlePrivacy') }}
+			</h1>
+			<br>
+			<p>
+				{{ $t('privacyP1') }}
+			</p>
+			<br>
+			<h2>
+				{{ $t('title1Privacy')}}
+			</h2>
+			<p>
+				{{ $t('SocialReasonPrivacy')}}<br>
+				{{ $t('NamePrivacy')}}<br>
+				{{ $t('AddresPrivacy')}}<br>
+				{{ $t('locatePrivacy')}}<br>
+				{{ $t('ProvincePrivacy')}}<br>
+				{{ $t('PostalCodePrivacy')}}<br>
+				{{ $t('CIFPrivacy')}}<br>
+				{{ $t('PhonePrivacy')}}<br>
+				{{ $t('EmailPrivacy')}}<br>
+				{{ $t('datePrivacy')}}<br>
+			</p>
+			<h2>
+				{{ $t('title2Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text2Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title3Privacy')}}
+			</h2>
+			<h3 class="pl-4">
+				{{ $t('title3_1Privacy')}}
+			</h3>
+			<p class="pl-4">
+				{{ $t('text3_1Privacy')}}
+		</p>
+			<h3 class="pl-4">
+				{{ $t('title3_2Privacy')}}
+			</h3>
+			<p class="pl-4">
+				{{ $t('text3_2Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title4Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text4Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title5Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text5Privacy')}}
+				<br>
+				{{ $t('text5list1Privacy')}}
+				<br>
+				{{ $t('text5list2Privacy')}}
+				<br>
+				{{ $t('textFinal5Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title6Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text6Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title7Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text7Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title8Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text8Privacy')}}
+			</p>
+			<h2>
+				{{ $t('title9Privacy')}}
+			</h2>
+			<p>
+				{{ $t('text9Privacy')}}
+			</p>
+		</div>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+h1 {
+	margin-top: 50px;
+	font-size: 50px;
+	font-weight: bold;
+	color: #EA621F;
+	text-align: center;
+}
+h2 {
+	margin-top: 20px;
+	font-size: 25px;
+	font-weight: bold;
+	color: #EA621F;
+}
+h3 {
+	margin-top: 20px;
+	font-size: 18px;
+	font-weight: bold;
+	color: #EA621F;
+}
+p {
+	margin-left: 18px;
+}
+</style>
