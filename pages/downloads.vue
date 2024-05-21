67ed43db0fe562ab33797d2d243ae21a28049c25
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-05-21 16:49:34 +0200

    feat: create download button

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -3,13 +3,17 @@
 		<div class="w-full flex justify-center pb-8 mt-10 text-center" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="300">
 			<FancyText class="text-primary text-6xl font-bold">Downloads</FancyText>
 		</div>
+		<DownloadButton />
 		<NuxtImg width="600" src="https://cdn.topgroups.travel/websites/assets/images/agencies.png" />
 		<NuxtImg width="600" src="https://cdn.topgroups.travel/websites/assets/images/hoteliers.png" />
 	</div>
 </template>
 
 <script>
+import DownloadButton from "~/components/DownloadButton.vue";
+
 export default {
-	name: "downloads"
+	name: "downloads",
+	components: {DownloadButton}
 }
 </script>
