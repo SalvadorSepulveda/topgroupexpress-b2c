cb1fff96de33fa26e75995c1cf0492cb64978477
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-05-27 15:14:58 +0200

    fix: responsive and dark mode

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<nav class="w-full fixed top-0 border-stroke dark:border-stroke-dark z-50" :class="navbarStyles">
+	<nav class="w-full fixed top-0 border-stroke dark:border-stroke-dark duration-300 z-50" :class="navbarStyles">
 		<div class="container max-w-[1400px] py-3 flex items-center justify-between">
 			<!-- Navbar logo -->
 			<navbar-logo :href="'/' + $i18n.locale" src="https://cdn.topgroups.travel/websites/assets/images/logo.png" alt="logo" :width="150" />
@@ -20,14 +20,14 @@
 				<NuxtLink
 					target="_blank"
 					to="https://hoteliers.topgroupexpress.com/"
-					class="hidden py-[10px] px-6 text-base font-medium hover:text-primary dark:text-white dark:hover:text-primary sm:inline-block whitespace-nowrap"
+					class="hidden py-[10px] px-6 text-base font-medium hover:text-primary dark:text-white dark:hover:text-primary xl:inline-block whitespace-nowrap"
 				>
 					{{ $t('appHoteliers') }}
 				</NuxtLink>
 				<NuxtLink
 					target="_blank"
 					to="https://hoteliers.topgroupexpress.com/"
-					class="hidden rounded-md bg-primary py-[10px] px-[30px] text-base font-medium text-white hover:bg-opacity-90 sm:inline-block whitespace-nowrap"
+					class="hidden rounded-md bg-primary py-[10px] px-[30px] text-base font-medium text-white hover:bg-opacity-90 xl:inline-block whitespace-nowrap"
 				>
 					{{ $t('appAgencies') }}
 				</NuxtLink>
@@ -37,9 +37,23 @@
 
 			<!-- Full-screen mobile menu that appears when burger button is clicked -->
 			<div class="absolute w-screen h-screen overflow-hidden xl:hidden inset-0 bg-white dark:bg-black bg-opacity-90 backdrop-blur-sm dark:bg-opacity-90 z-40 flex flex-col items-center justify-center " v-if="menuOpen">
-				<generic-icon name="cross" width="45" @click="closeMenu" class="absolute top-10 right-10 text-[#181C31] hover:text-primary dark:text-white dark:hover:text-primary cursor-pointer"/>
-				<ul class="text-center">
-					<li v-for="link in links" :key="link.label" class="mb-8">
+				<generic-icon name="cross" width="45" @click="closeMenu" class="absolute top-3 right-3 text-[#181C31] hover:text-primary dark:text-white dark:hover:text-primary cursor-pointer"/>
+				<ul class="flex flex-col gap-4 items-center">
+					<NuxtLink
+						target="_blank"
+						to="https://hoteliers.topgroupexpress.com/"
+						class="rounded-md bg-primary py-[10px] px-[30px] text-2xl font-medium text-white dark:text-white text-gray-800 text-nowrap"
+					>
+						{{ $t('appAgencies') }}
+					</NuxtLink>
+					<NuxtLink
+						target="_blank"
+						to="https://hoteliers.topgroupexpress.com/"
+						class="pb-10 text-2xl font-medium dark:text-white text-gray-800 text-nowrap"
+					>
+						{{ $t('appHoteliers') }}
+					</NuxtLink>
+					<li v-for="link in links" :key="link.label">
 						<NuxtLink :to="link.route" class="text-2xl font-medium dark:text-white text-gray-800" @click="closeMenu">
 							{{ $t(link.label) }}
 						</NuxtLink>
@@ -58,7 +72,6 @@ export default {
 	data: () => ({
 		menuOpen: false,
 		links: [
-			{ label: 'feature', route: '#features' },
 			{ label: 'contact', route: '#contact' },
 			{ label: 'webinars', route: '/webinars' },
 			{ label: 'downloads', route: '/downloads' },
--- components/navbar/langSwitcher.vue
+++ components/navbar/langSwitcher.vue
@@ -1,19 +1,19 @@
 <template>
-	<div class="flex flex-col relative font-medium dark:text-white text-sm duration-300 text- w-[160px]">
-		<button class="cursor-pointer flex justify-between p-1 rounded-lg items-center border border-transparent gap-2 w-full hover:bg-[#F8F9FF] dark:hover:bg-dark" @click="toggle">
+	<div class="relative font-medium dark:text-white h-[44px] text-sm w-fit xs:w-[170px]">
+		<button class="cursor-pointer flex justify-between h-full rounded-lg items-center border border-transparent gap-2 w-full hover:bg-[#F8F9FF] dark:hover:bg-dark px-2" @click="toggle">
 			<span class="flex items-center gap-2">
 				<generic-icon width="28" :name="$i18n.locale" alt="'flag-' + $i18n.locale" />
-				{{ currentLanguageName }}
+				<span class="hidden xs:inline-block">{{ currentLanguageName }}</span>
 			</span>
-			<generic-icon v-if="!open" width="20" name="chevronDown" alt="chevronDown" />
-			<generic-icon v-else width="20" name="chevronUp" alt="chevronUp" />
+			<generic-icon v-if="!open" width="20" name="chevronDown" alt="chevronDown"/>
+			<generic-icon v-else width="20" name="chevronUp" alt="chevronUp"/>
 		</button>
-		<div v-if="open" class="z-50 bg-white dark:bg-black absolute flex flex-col top-11 shadow rounded-lg w-full border border-stroke dark:border-stroke-dark overflow-hidden">
+		<div v-if="open" class="z-50 bg-white dark:bg-black duration-300 absolute flex flex-col top-12 shadow rounded-lg w-full border border-stroke dark:border-stroke-dark overflow-hidden">
 			<button
-				class="cursor-pointer flex p-1 items-center gap-2 hover:bg-[#F8F9FF] dark:hover:bg-dark last-of-type:border-none border-b border-stroke dark:border-stroke-dark"
+				class="cursor-pointer flex p-2 items-center w-full justify-center xs:justify-start gap-2 hover:bg-[#F8F9FF] dark:hover:bg-dark"
 				v-for="locale in availableLocales" :key="locale.code" @click="setLanguage(locale)"
 			>
-				<generic-icon width="28" :name="locale.code" alt="'flag-' + locale.code" /> {{ locale.name }}
+				<generic-icon width="28" :name="locale.code" alt="'flag-' + locale.code" /> <span class="hidden xs:inline-block">{{ locale.name }}</span>
 			</button>
 		</div>
 	</div>
