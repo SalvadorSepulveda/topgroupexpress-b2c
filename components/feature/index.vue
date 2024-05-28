9ed860338e72098a925877b3daec61d698df9a61
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-05-28 12:31:12 +0200

    refactor: icons in icons.js

--- components/feature/index.vue
+++ components/feature/index.vue
@@ -2,7 +2,7 @@
 	<section id="feature" class="relative px-4 pt-[110px]">
 		<generic-title :title="$t('featureTitle')" :subtitle="$t('featureDescription')" />
 		<div class="container max-w-[1390px] flex flex-wrap rounded-2xl bg-white pt-14 pb-14 shadow-card dark:bg-dark dark:shadow-card-dark md:pb-1 lg:pt-20 lg:pb-5 xl:px-10 duration-300" >
-			<FeatureItem v-for="feature in features" :key="feature.title" :title="$t(feature.title)" :description="$t(feature.description)" :icon="feature.icon" />
+			<feature-item v-for="feature in features" :key="feature.title" :title="$t(feature.title)" :description="$t(feature.description)" :icon="feature.icon" />
 		</div>
 		<generic-icon name="shadowRight" class="absolute top-0 right-0 -z-10 text-[#BEB3FD]" />
 		<generic-icon name="shadowLeft" class="absolute left-0 -bottom-1/2 -z-10 hidden md:block text-[#FFC960]" />
@@ -17,32 +17,32 @@ export default {
 			{
 				title: 'featureApps',
 				description: 'appsDescription',
-				icon: 'IconApp',
+				icon: 'app',
 			},
 			{
 				title: 'featurePrices',
 				description: 'pricesDescription',
-				icon: 'IconLayers',
+				icon: 'layers',
 			},
 			{
 				title: 'featureDashboard',
 				description: 'dashboardDescription',
-				icon: 'IconLayout',
+				icon: 'layout',
 			},
 			{
 				title: 'featureAgility',
 				description: 'agilityDescription',
-				icon: 'IconSpeed',
+				icon: 'speed',
 			},
 			{
 				title: 'featureReservations',
 				description: 'reservationsDescription',
-				icon: 'IconSliders',
+				icon: 'sliders',
 			},
 			{
 				title: 'featureUpdates',
 				description: 'updatesDescription',
-				icon: 'IconReload',
+				icon: 'reload',
 			},
 		]
 	})
