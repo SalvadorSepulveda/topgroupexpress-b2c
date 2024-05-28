bf72f6b97c09052f9e7ffcfec47b2ae63f9b5579
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-05-28 15:59:03 +0200

    fix: responsive in carousel

--- components/screens/item.vue
+++ components/screens/item.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="swiper-slide">
-		<div class="mx-auto w-full max-w-[252px] xs:max-w-[265px]">
+		<div class="mx-auto w-full max-w-[250px] xs:max-w-[265px] md:max-w-[215px] lg:max-w-[265px]">
 			<NuxtImg loading="lazy" format="png" :src="src" class="mx-auto max-w-full w-full" alt="screen-1-light" />
 		</div>
 	</div>
