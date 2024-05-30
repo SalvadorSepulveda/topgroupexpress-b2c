64a0e115b7586152ce5e407d0d1290de24e5abb7
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-05-30 13:57:38 +0200

    refactor: images

--- components/about/index.vue
+++ components/about/index.vue
@@ -6,20 +6,20 @@
 				:bigtitle="$t('bigTitleAgencies')"
 				:description="$t('descriptionAgencies')"
 				:list="[$t('oneListAgencies'), $t('twoListAgencies'), $t('threeListAgencies'), $t('fourListAgencies')]"
+				image="https://cdn.t3s.es/website/screens/agencies.webp"
+				alt="screen-agencies"
 				left
-			>
-				<NuxtImg format="webp,avif,jpeg" src="https://cdn.topgroups.travel/websites/assets/images/mok.png" class="mx-auto w-[300px] h-[600px]" alt="screenagency"/>
-			</about-card>
+			/>
 			<about-card
 				class="pt-32"
 				:smalltitle="$t('smallTitleHoteliers')"
 				:bigtitle="$t('bigTitleHoteliers')"
 				:description="$t('descriptionHoteliers')"
 				:list="[$t('oneListHoteliers'), $t('twoListHoteliers'), $t('threeListHoteliers'), $t('fourListHoteliers')]"
+				image="https://cdn.t3s.es/website/screens/hoteliers.webp"
+				alt="screen-hoteliers"
 				right
-			>
-				<NuxtImg format="webp,avif,jpeg" src="https://cdn.topgroups.travel/websites/assets/images/mok.png" class="mx-auto w-[300px] h-[600px]"  alt="screen-hotelier"/>
-			</about-card>
+			/>
 		</div>
 		<div class="absolute right-0 top-36 -z-10">
 			<generic-icon name="circleRight" />
--- components/screens/index.vue
+++ components/screens/index.vue
@@ -2,8 +2,8 @@
 	<section id="screens" class="relative z-20 pt-[210px] pb-[100px]">
 		<generic-title :title="$t('titleScreens')" :subtitle="$t('descriptionScreens')" />
 		<div class="container mx-auto max-w-[1000px] swiper mySwiper relative z-20">
-			<div class="absolute top-0 left-0 right-0 z-50 mx-auto w-full md:w-1/3">
-				<NuxtImg loading="lazy" format="png" src="https://cdn.t3s.es/website/screens/frame.webp" width="295" class="mx-auto" alt="mobile-frame" />
+			<div class="absolute top-0 left-0 right-0 z-50 mx-auto w-full md:w-1/3 max-w-[295px] md:max-w-[240px] lg:max-w-[295px]">
+				<NuxtImg loading="lazy" format="png" src="https://cdn.t3s.es/website/screens/frame.webp" class="mx-auto w-full" alt="mobile-frame" />
 			</div>
 			<div class="swiper-wrapper py-2">
 				<screens-item v-for="(screen, index) in screens" :key="index" :src="screen"/>
