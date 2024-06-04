b254e0df2707865ce35521bdeca7f2af64e4550c
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-06-04 13:06:08 +0200

    fix: transition color genetic title

--- components/generic/title.vue
+++ components/generic/title.vue
@@ -2,10 +2,10 @@
 	<div class="container">
 		<div class="mx-auto mb-14 max-w-[690px] text-center lg:mb-[70px]" >
 			<h2 class="mb-4 text-3xl font-bold text-black dark:text-white sm:text-4xl md:text-[44px] md:leading-tight" data-aos="fade-up" data-aos-delay="100" >
-				{{ title }}
+				<span class="duration-300">{{ title }}</span>
 			</h2>
-			<p v-if="subtitle" class="text-base text-bodywhite dark:text-body duration-300" data-aos="fade-up" data-aos-delay="200">
-				{{ subtitle }}
+			<p v-if="subtitle" class="text-bodywhite dark:text-body duration-300" data-aos="fade-up" data-aos-delay="200">
+				<span class="duration-300">{{ subtitle }}</span>
 			</p>
 		</div>
 	</div>
