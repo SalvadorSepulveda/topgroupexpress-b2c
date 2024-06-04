d508093ddc489fcf51cb64d2900b00f607cec3fc
Salvador Sepúlveda Millán <contacto@sepu.dev>
2024-06-04 13:08:48 +0200

    fix: transition color feature

--- components/feature/index.vue
+++ components/feature/index.vue
@@ -1,8 +1,10 @@
 <template>
 	<section id="feature" class="relative px-4 pt-[110px]">
 		<generic-title :title="$t('featureTitle')" :subtitle="$t('featureDescription')" />
-		<div class="container max-w-[1390px] flex flex-wrap rounded-2xl bg-white pt-14 pb-14 shadow-card dark:bg-dark dark:shadow-card-dark md:pb-1 lg:pt-20 lg:pb-5 xl:px-10 duration-300" data-aos="fade-up" data-aos-delay="200" >
-			<feature-item v-for="feature in features" :key="feature.title" :title="$t(feature.title)" :description="$t(feature.description)" :icon="feature.icon" :data-aos="feature.animation" :data-aos-delay="feature.delay" />
+		<div class="container max-w-[1390px]" data-aos="fade-up" data-aos-delay="200" >
+			<div class="flex flex-wrap rounded-2xl bg-white pt-14 pb-14 shadow-card dark:bg-dark dark:shadow-card-dark md:pb-1 lg:pt-20 lg:pb-5 xl:px-10 duration-300">
+				<feature-item v-for="feature in features" :key="feature.title" :title="$t(feature.title)" :description="$t(feature.description)" :icon="feature.icon" :data-aos="feature.animation" :data-aos-delay="feature.delay" />
+			</div>
 		</div>
 		<generic-icon name="shadowRight" class="absolute top-0 right-0 -z-10 text-[#BEB3FD]" data-aos="fade-left" data-aos-delay="100" />
 		<generic-icon name="shadowLeft" class="absolute left-0 -bottom-1/2 -z-10 hidden md:block text-[#FFC960]" data-aos="fade-right" data-aos-delay="100" />
